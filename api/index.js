3a41bac9fb7c42536e5310c87849a78144e5d364
Uliana Nadorozhna <102276922+UlianaNad@users.noreply.github.com>
2024-04-23 15:18:27 +0300

    connected to ws server

--- api/index.js
+++ api/index.js
@@ -5,14 +5,13 @@ import { UserModel } from "./models/User.js";
 import jwt from "jsonwebtoken";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import bcrypt from 'bcryptjs'
-
+import bcrypt from "bcryptjs";
+import { WebSocketServer } from "ws";
 mongoose.connect(process.env.MONGO_URL);
 
 const jwtSecret = process.env.JWT_SECRET;
-const bcryptSalt = bcrypt.genSaltSync(10)
+const bcryptSalt = bcrypt.genSaltSync(10);
 const app = express();
-
 
 app.use(
   cors({
@@ -23,44 +22,49 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-
-app.get('/current', (req,res) => {
+app.get("/current", (req, res) => {
   const token = req.cookies?.token;
-  if(token){
-    jwt.verify(token, jwtSecret, {} , (error, userData)=>{
-      if(error) throw error;
-  
-      res.json(userData)
-    })
-  } else{
-    res.status(401).json('no token')
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, (error, userData) => {
+      if (error) throw error;
+
+      res.json(userData);
+    });
+  } else {
+    res.status(401).json("no token");
   }
- 
 });
 
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  const foundUser = await UserModel.findOne({username})
+  const foundUser = await UserModel.findOne({ username });
 
-  if(foundUser){
-    const passwordOk = bcrypt.compareSync(password, foundUser.password)
-    if(passwordOk){
-      jwt.sign({ userId: foundUser._id, username }, jwtSecret, {}, (error, token) => {
-        if (error) throw error;
-  
-        res.cookie("token", token).status(200).json({ _id: foundUser._id });
-      });
+  if (foundUser) {
+    const passwordOk = bcrypt.compareSync(password, foundUser.password);
+    if (passwordOk) {
+      jwt.sign(
+        { userId: foundUser._id, username },
+        jwtSecret,
+        {},
+        (error, token) => {
+          if (error) throw error;
+
+          res.cookie("token", token).status(200).json({ _id: foundUser._id });
+        }
+      );
     }
   }
-
 });
 
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
   try {
-    const hashedPassword = bcrypt.hashSync(password, bcryptSalt)
+    const hashedPassword = bcrypt.hashSync(password, bcryptSalt);
 
-    const User = await UserModel.create({ username:username, password:hashedPassword });
+    const User = await UserModel.create({
+      username: username,
+      password: hashedPassword,
+    });
 
     jwt.sign({ userId: User._id, username }, jwtSecret, {}, (error, token) => {
       if (error) throw error;
@@ -72,5 +76,38 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.listen(4040);
+const server = app.listen(4040);
 
+const wss = new WebSocketServer({ server });
+wss.on("connection", (connection, req) => {
+  console.log("connected");
+  const cookies = req.headers.cookie;
+  if (cookies) {
+    const tokenCookieString = cookies
+      .split(";")
+      .find((str) => str.startsWith("token"));
+
+    if (tokenCookieString) {
+      const token = tokenCookieString.split("=")[1];
+      if (token) {
+        jwt.verify(token, jwtSecret, {}, (err, userData) => {
+          if (err) throw err;
+          const { userId, username } = userData;
+          connection.userId = userId;
+          connection.username = username;
+        });
+      }
+    }
+  }
+
+  [...wss.clients].forEach((client) => {
+    client.send(
+      JSON.stringify(
+        {onLine: [...wss.clients].map((c) => ({
+          userId: c.userId,
+          username: c.username,
+        }))}
+      )
+    );
+  });
+});
